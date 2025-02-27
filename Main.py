ecd671ac513296a447e3c0d9e169077ae2fd9eb4
Duncan Staats <staatsd@wlhs.wlwv.k12.or.us>
2025-02-27 17:48:00 +0000

    news

--- Main.py
+++ Main.py
@@ -5,28 +5,30 @@ def fun_1(province, region):
     region = {"Urban": "123456789", "Rural": "0"}
     return province, region
 
-def fun_2(province,region):
-    while True:
+def fun_2(province, region):
+    while True: 
         code = input("Enter in your postal code: ")
         code = code.split()
-        for i in range(len(code)):
-            if code[0][0] == province and code[0][1] == region:
-                print(code)
-                break
-            else:
-                print("This postal code is invalid and cannot be used. Please enter in an accurate code.")
-                continue
+        if code[0][0] == province.values() and code[0][1] == region.values():
+            break
+        else:
+            print("This postal code is invalid and does not have the proper order of characters. Please enter in an accurate code.")
+            continue
+""" 
+check after asked and before split, if code length == 6 then break, else prompt user to enter in the correct amount of characters.
+check if it is: character digit, charctaer, space, digit, charcter, digit
+"""
 
 def fun_3(province, region):
-    """
-    find out the characters in the list and print the corresponing dictionary key to the value
-    get rid of whitespace
-    cehck for 6 charachtesrs
-    """
+    pass
+"""
+find out the characters in the list and print the corresponing dictionary key to the value
+"""
 
 
 def main():
-    pass
+    fun_2(province=any, region=any)
+    fun_3(province=any, region=any)
 
 if __name__ == '__main__':
     main()
