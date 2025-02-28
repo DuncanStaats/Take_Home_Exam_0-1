4afea0406d541c00c9ecc867ffb7962d6ad6cf04
Duncan Staats <staatsd@wlhs.wlwv.k12.or.us>
2025-02-28 18:12:58 +0000

    nh

--- Main.py
+++ Main.py
@@ -1,34 +1,43 @@
-def fun_1(province, region):
-    province = {"Newfoundland": "A", "Nova Scotia": "B", "Prince Edward Island": "C", "New Brunswick": "E",
-                "Quebec": "GHJ", "Ontario": "KLMNP", "Manitoba": "R", "Saskatchewan": "S","Alberta": "T", 
-                "British Columbia": "V", "Nunavat": "X", "Northwest Territories": "X", "Yukon": "Y"}
-    region = {"Urban": "123456789", "Rural": "0"}
-    return province, region
+def fun_1():
+    province = {
+        "Newfoundland": ["A"], 
+        "Nova Scotia": ["B"], 
+        "Prince Edward Island": ["C"], 
+        "New Brunswick": ["E"],
+        "Quebec": ["G", "H", "J"], 
+        "Ontario": ["K", "L", "M", "N", "P"], 
+        "Manitoba": ["R"], 
+        "Saskatchewan": ["S"],
+        "Alberta": ["T"], 
+        "British Columbia": ["V"], 
+        "Nunavut": ["X"], 
+        "Northwest Territories": ["X"], 
+        "Yukon": ["Y"]
+    }
+    return province
 
-def fun_2(province, region):
+def fun_2(province):
+    check_1 = {char for chars in province.values() for char in chars}
+    
     while True: 
-        code = input("Enter in your postal code: ")
-        code = code.split()
-        if code[0][0] == province.values() and code[0][1] == region.values():
-            break
+        code = input("Enter your postal code: ")
+        code = code.strip()
+        code = code.title()
+        
+        if len(code) == 7 and code[3] == ' ':
+            if code[0] in check_1:
+                region = "Urban" if code[1] != '0' else "Rural"
+                name = next(name for name, chars in province.items() if code[0] in chars)
+                print(f"The postal code is for a {region} region in {name}.")
+                break
+            else:
+                print("Invalid starting character. Please enter a valid postal code.")
         else:
-            print("This postal code is invalid and does not have the proper order of characters. Please enter in an accurate code.")
-            continue
-""" 
-check after asked and before split, if code length == 6 then break, else prompt user to enter in the correct amount of characters.
-check if it is: character digit, charctaer, space, digit, charcter, digit
-"""
-
-def fun_3(province, region):
-    pass
-"""
-find out the characters in the list and print the corresponing dictionary key to the value
-"""
-
+            print("Incorrect format. It should be: Letter-Digit-Letter Space Digit-Letter-Digit.")
 
 def main():
-    fun_2(province=any, region=any)
-    fun_3(province=any, region=any)
+    province = fun_1()
+    fun_2(province)
 
 if __name__ == '__main__':
     main()
